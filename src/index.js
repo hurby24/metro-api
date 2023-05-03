73beb594fdcc05d9891206abeaddeda11473e2aa
Hebib Qurbanov <qurbanovhebib554@gmail.com>
2023-05-04 00:12:25 +0400

    fixed line and code router

--- src/index.js
+++ src/index.js
@@ -10,34 +10,67 @@ export default {
   fetch(request) {
     try {
       const url = new URL(request.url);
-      switch (url.pathname) {
-        case "/":
-          return new Response("This is a simple API to get metros by ID.", {
-            headers: { ...headers, "content-type": "text/plain" },
-          });
-        case "/metro":
-          return new Response(JSON.stringify(metros), {
-            headers: { ...headers, "content-type": "application/json" },
-          });
-        case `/metro/${url.pathname.split("/")[2]}`:
-          const id = parseInt(url.pathname.split("/")[2]);
-          const metro = metros.find((metro) => metro.id === id);
-          if (metro) {
-            return new Response(JSON.stringify(metro), {
+
+      if (url.pathname === "/") {
+        return new Response("This is a simple API to get metros by ID.", {
+          headers: { ...headers, "content-type": "text/plain" },
+        });
+      }
+
+      if (url.pathname === "/metro") {
+        return new Response(JSON.stringify(metros), {
+          headers: { ...headers, "content-type": "application/json" },
+        });
+      }
+
+      if (
+        url.pathname.startsWith("/metro/") &&
+        url.pathname.split("/").length === 4
+      ) {
+        const line = url.pathname.split("/")[2].toLowerCase();
+        const id = url.pathname.split("/")[3].toLowerCase();
+        const foundLine = metros.lines.find((l) => l.name === line);
+
+        if (foundLine) {
+          const foundStation = foundLine.stations.find((s) => s.code === id);
+
+          if (foundStation) {
+            return new Response(JSON.stringify(foundStation), {
               headers: { ...headers, "content-type": "application/json" },
             });
           } else {
-            return new Response(JSON.stringify("metro not found"), {
+            return new Response(JSON.stringify("Station not found"), {
               status: 404,
               headers: { ...headers, "content-type": "text/plain" },
             });
           }
-        default:
-          return new Response("Not Found", {
+        } else {
+          return new Response(JSON.stringify("Line not found"), {
             status: 404,
             headers: { ...headers, "content-type": "text/plain" },
           });
+        }
       }
+      if (url.pathname.startsWith("/metro/")) {
+        const line = url.pathname.split("/")[2].toLowerCase();
+        const foundLine = metros.lines.find((l) => l.name === line);
+
+        if (foundLine) {
+          return new Response(JSON.stringify(foundLine.stations), {
+            headers: { ...headers, "content-type": "application/json" },
+          });
+        } else {
+          return new Response(JSON.stringify("Line not found"), {
+            status: 404,
+            headers: { ...headers, "content-type": "text/plain" },
+          });
+        }
+      }
+
+      return new Response("Not Found", {
+        status: 404,
+        headers: { ...headers, "content-type": "text/plain" },
+      });
     } catch (error) {
       return new Response(error.toString(), {
         status: 500,
